d5366c2bc505fb47957dce12c6ddfd36e37cc320
wu <timmy61501@gmail.com>
2024-01-22 11:21:34 +0800

    完成最新消息前台顯示功能及跑馬燈

---
+++ front/news.php
@@ -0,0 +1,12 @@
+<h2 class="ct">最新消息</h2>
+<table class="all ct">
+    <tr class="tt ct">
+        <td>標題</td>
+    </tr>
+    <tr class="pp">
+        <td>情人節特惠活動</td>
+    </tr>
+    <tr class="pp">
+        <td>年終特賣會開跑了</td>
+    </tr>
+</table>
