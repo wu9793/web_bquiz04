1a860f1ec743bf72f1d3dd021c03ca9bdc21415a
wu <timmy61501@gmail.com>
2024-01-22 11:01:41 +0800

    建立後台頁尾版權管理頁面

---
+++ back/bot.php
@@ -0,0 +1,15 @@
+<h2 class="ct">編輯頁尾版權區</h2>
+<form action="?" method="post">
+    <table class="all">
+        <tr>
+            <td class="tt">頁尾宣告內容</td>
+            <td class="pp">
+                <input type="text" name="bottom" value="<?= $Bottom->find(1)['bottom']; ?>">
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="編輯">
+        <input type="reset" value="重置">
+    </div>
+</form>
