64efa4435225923625c4e4f9475153d568ee9724
wu <timmy61501@gmail.com>
2024-01-31 11:44:35 +0800

    建立後台商品分類與商品管理頁面版型

---
+++ back/th.php
@@ -0,0 +1,53 @@
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+    新增大分類
+    <input type="text" name="big" id="big">
+    <button>新增</button>
+</div>
+<div class="ct">
+    新增中分類
+    <select name="big" id="bigs"></select>
+    <input type="text" name="mid" id="mid">
+    <button>新增</button>
+</div>
+
+<table class="all">
+    <tr class="tt">
+        <td>流行皮件</td>
+        <td class="ct">
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+    <tr class="pp ct">
+        <td>女用皮件</td>
+        <td>
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+</table>
+
+<h2 class="ct">商品管理</h2>
+<div class="ct"><button>新增商品</button></div>
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>庫存量</td>
+        <td>狀態</td>
+        <td>操作</td>
+    </tr>
+    <tr class="pp">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+            <button>修改</button>
+            <button>刪除</button>
+            <button>上架</button>
+            <button>下架</button>            
+        </td>
+    </tr>
+</table>
