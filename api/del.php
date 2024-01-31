e7038a9cf69c1062f2aa92e633691729ca9ffeb0
wu <timmy61501@gmail.com>
2024-01-31 10:24:27 +0800

    完成後台管理員刪除功能

---
+++ api/del.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once "db.php";
+$db=new DB($_POST['table']);
+$db->del($_POST['id']);
+?>
--- back/admin.php
+++ back/admin.php
@@ -1,3 +1,39 @@
 <div class="ct">
     <button onclick="location.href='?do=add_admin'">新增管理員</button>
 </div>
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="tt ct">密碼</td>
+        <td class="tt ct">管理</td>
+    </tr>
+    <?php
+    $rows=$Admin->all();
+    foreach($rows as $row){
+    ?>
+    <tr>
+        <td class="pp ct"><?=$row['acc'];?></td>
+        <td class="pp ct"><?=str_repeat("*",strlen($row['pw']));?></td>
+        <td class="pp ct">
+            <?php
+            if($row['acc']=='admin'){
+                echo "此帳號為最高權限";
+            }else{
+                echo "<button onclick='location.href=&#39;?do=edit_admin&id={$row['id']}&#39;'>修改</button>";
+                echo "<button onclick='del(&#39;admin&#39;,{$row['id']})'>刪除</button>";
+            }
+            ?>
+        </td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+<div class="ct"><button onclick="location.href='index.php'">返回</button></div>
+<script>
+    function del(table,id){
+        $.post("./api/del.php",{table,id},()=>{
+            location.reload();
+        })
+    }
+</script>
