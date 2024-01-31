8cd21242a389f4acb186478b4d96347b276354be
wu <timmy61501@gmail.com>
2024-01-31 10:32:43 +0800

    將del()搬到js.js,同時調整jquery引入在head

--- back/admin.php
+++ back/admin.php
@@ -30,10 +30,3 @@
     ?>
 </table>
 <div class="ct"><button onclick="location.href='index.php'">返回</button></div>
-<script>
-    function del(table,id){
-        $.post("./api/del.php",{table,id},()=>{
-            location.reload();
-        })
-    }
-</script>
