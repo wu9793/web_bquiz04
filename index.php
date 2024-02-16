4471e948ed41aa6c7a7708fa3b88c2be6af1d8be
wu <timmy61501@gmail.com>
2024-02-16 11:11:40 +0800

    在分類選單連結加上分類的id

--- index.php
+++ index.php
@@ -53,20 +53,20 @@
                 </div>
                 <div id="left" class="ct">
                         <div style="min-height:400px;">
-                                <a href="">全部商品</a>
+                                <a href="">全部商品(<?=$Goods->count(['sh'=>1]);?>)</a>
                                 <?php
-                                $bigs=$Types->all(['big_id'=>0]);
+                                $bigs=$Type->all(['big_id'=>0]);
                                 foreach($bigs as $big){
                                 ?>
                                 <div class="ww">
-                                        <a href=""><?=$big['name'];?></a>
+                                        <a href="?type=<?=$big['id'];?>"><?=$big['name'];?><?=$Goods->count(['sh'=>1,'big'=>$big['id']]);?></a>
                                         <div class="s">
                                                 <?php
                                                 if($Type->count(["big_id"=>$big['id']])>0){
                                                         $mids=$Type->all(["big_id"=>$big['id']]);
                                                         foreach($mids as $mid){
                                                 ?>
-                                                        <a href=""><?=$mid['name'];?></a>
+                                                        <a href="?type=<?=$mid['id'];?>"><?=$mid['name'];?>(<?=$Goods->count(['sh'=>1,'mid'=>$mid['id']])?>)</a>
                                                 <?php          
                                                         }
                                                 }
