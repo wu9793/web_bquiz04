7d79c68776805671b5d8cf9182aa970f06b0baef
wu <timmy61501@gmail.com>
2024-02-16 11:37:32 +0800

    完成首頁顯示商品分類導航文字

--- front/main.php
+++ front/main.php
@@ -1,1 +1,18 @@
-<button onclick="location.href='?do=buycart'">我要購買</button>
+<?php
+$type=$_GET['type']??0;
+$nav='';
+
+if($type==0){
+    $nav="全部商品";
+}else{
+    $t=$Type->find($type);
+    if($t['big_id']==0){
+        $nav=$t['name'];
+    }else{
+        $b=$Type->find($t['big_id']);
+        $nav=$b['name'] ." > ". $t['name'];
+    }
+}
+?>
+
+<h2><?=$nav;?></h2>
